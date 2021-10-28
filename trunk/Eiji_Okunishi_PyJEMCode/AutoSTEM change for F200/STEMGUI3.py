0d6fefe7d157686e2ae7f04f3c4f70b40e65b21e
Eiji Okunishi <okunishi@jeol.co.jp>
2021-10-28 14:09:37 +0900

    change foldername

--- trunk/Eiji_Okunishi_PyJEMCode/AutoSTEM change for F200/STEMGUI3.py
+++ trunk/Eiji_Okunishi_PyJEMCode/AutoSTEM change for F200/STEMGUI3.py
@@ -3,6 +3,22 @@
 # Form implementation generated from reading ui file 'C:/Users/okunishi/PyJEMscript_in_okunishicalcPC/PyJEM_okunishi/PyJEMscript NEW/F200_change_STEM/STEMGUI3.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
+"""
+ Copyright (C) <2021/10/25>  <Eiji Okunishi>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""    
 #
 # WARNING! All changes made in this file will be lost!
 
